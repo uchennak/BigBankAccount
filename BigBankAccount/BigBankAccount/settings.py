5a69a2fb0991a1604d38bb73605674918befbdab
uchennak <uchennakamalu@gmail.com>
2020-12-23 22:54:31 -0500

    yoodles

--- BigBankAccount/BigBankAccount/settings.py
+++ BigBankAccount/BigBankAccount/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '17fru6@!e*@r3j72vw$jcl@g)5dyihn4dv$w#otltorehy%%_&'
+SECRET_KEY = 'yoodles'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
