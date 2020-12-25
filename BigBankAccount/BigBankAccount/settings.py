2a4cac5068f75262dcda35643f8dd8ecec351892
uchennak <uchennakamalu@gmail.com>
2020-12-25 17:29:21 -0500

    small changes

--- BigBankAccount/BigBankAccount/settings.py
+++ BigBankAccount/BigBankAccount/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yoodles'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
